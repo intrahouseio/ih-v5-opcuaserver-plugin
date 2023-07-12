1cc7a729e8594b08f58d82f7a6f3f1051789bcae
goodspeed1986 <goodspeed1986@gmail.com>
2023-07-12 13:16:28 +0300

    onChange add

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 const util = require('util');
 const tools = require('./tools');
-const { OPCUAServer, Variant, DataType, StatusCodes } = require("node-opcua");
+const { OPCUAServer, Variant, DataType, StatusCodes, makeAccessLevelFlag } = require("node-opcua");
 
 module.exports = async function (plugin) {
   const params = plugin.params;
+  const locations = await plugin.places.get();
+  const locationsObj = {}
+  locations.forEach(item => {
+    locationsObj[item.location] = item.title;
+  })
   let extraChannels = await plugin.extra.get();
   let filter = await filterExtraChannels(extraChannels);
+  
+  plugin.log("locationsObj " + util.inspect(locationsObj, null, 4));
   const server = new OPCUAServer({
     port: parseInt(params.port) || 4334, // the port of the listening socket of the server
     resourcePath: params.sourcepath || "/UA/IntraServer", // this path will be added to the endpoint resource name
     buildInfo: {
       productName: "IntraServer",
-      buildNumber: "7658",
-      buildDate: new Date(2014, 5, 2)
+      buildNumber: "5.0.0",
+      buildDate: new Date(2023, 6, 29)
     }
   });
   await server.initialize();
@@ -21,87 +28,211 @@ module.exports = async function (plugin) {
   const namespace = addressSpace.getOwnNamespace();
   const objectsFolder = addressSpace.rootFolder.objects;
 
-
+  const devicesNode = namespace.addFolder(objectsFolder, { browseName: "Devices", nodeId: "s=Devices" });
+  const locationNode = namespace.addFolder(objectsFolder, { browseName: "Location", nodeId: "s=Location" });
+  const tagNode = namespace.addFolder(objectsFolder, { browseName: "Tag", nodeId: "s=Tag" });
   // declare a new objects
-  const devicesNode = namespace.addFolder(objectsFolder, { browseName: "Devices" });
-  const locationNode = namespace.addFolder(objectsFolder, { browseName: "Location" });
-  const tagNode = namespace.addFolder(objectsFolder, { browseName: "Tag" });
 
-
-  addOpcDevices(filter.devicesarr, devicesNode);
-  addOpcDevices(filter.tagarr, tagNode);
-  addOpcDevices(filter.locationarr, locationNode);
- 
+  addOpcTag(filter.tagobj);
+  addOpcLocation(filter.locationobj);
+  addOpcDevices(filter.devicesobj);
   subExtraChannels(filter);
 
-  function addOpcDevices(arr, node) {
+  function addOpcLocation(locationobj) {
+    plugin.log('locationobj = ' + util.inspect(locationobj, null, 4));
+    if (locationobj != undefined) {
+      for (const prop in locationobj) {
+        let maxDepth = 1;
+        const locationArr = Object.keys(locationobj[prop]).sort();
+        
+        locationArr.forEach(location => {
+          let depth = getDepth(location);
+          locationobj[prop][location].depth = depth;
+          if (depth > maxDepth) maxDepth = depth;
+          let xloc = location;
+          while (depth > 1) {
+            const parentLocation = getParentLocation(xloc);
+            depth = getDepth(parentLocation);
+            // Если есть пропущенные уровни - добавить как элементы без устройств '/place/dg003/dg010':{depth:2}
+            if (!locationobj[prop][parentLocation]) locationobj[prop][parentLocation] = { depth };
+            xloc = parentLocation;
+          }
+        });
+        plugin.log('1 locationobj[prop] = ' + util.inspect(locationobj[prop], null, 4));
 
+        for (let i = 1; i <= maxDepth; i++) {
+          Object.keys(locationobj[prop]).forEach(location => {
+            if (locationobj[prop][location].depth == i) {
+              let parentNode;
+              if (i == 1) {
+                parentNode = locationNode;
+              } else {
+                const parentLocation = getParentLocation(location);
+                parentNode = locationobj[prop][parentLocation].node;
+              }
+              const curNode = namespace.addFolder(parentNode, {
+                browseName: locationsObj[location],
+                nodeId: "s=" + location
+              });
+
+              // в элемент locationobj добавить ссылку на добавленный folder: { ref: [ ], depth: 1, node:<curNode>  },
+              // Нужно будет на следующем уровне
+              locationobj[prop][location].node = curNode;
+              plugin.log('Before add opObjects = ' + util.inspect(location, null, 4));
+              plugin.log('Before add opObjects ref = ' + util.inspect(locationobj[prop][location].ref, null, 4));
+              plugin.log('Before add opObjects node = ' + util.inspect(curNode, null, 4));
+              // Если есть устройства - их добавить
+              if (locationobj[prop][location].ref) addOpcObjects(locationobj[prop][location].ref, curNode, location);
+
+              plugin.log("After add opObjects "  + location)
+            }
+          });
+        }
+      }
+    }
+  }
+
+  function addOpcDevices(devicesobj) {
+    if (devicesobj != undefined) {
+      addOpcObjects(devicesobj.Devices, devicesNode, "Devices");
+    }
+  }
+
+  function addOpcTag(tagobj) {
+    if (tagobj != undefined) {
+      for (const prop in tagobj) {
+        const curNode = namespace.addFolder(tagNode, {
+          browseName: prop,
+          nodeId: "s=" + prop
+        });
+        addOpcObjects(tagobj[prop], curNode, prop);
+      }
+    }
+  }
+
+  function addOpcObjects(arr, node, nodeName) {
     arr.forEach(item => {
       const device = namespace.addObject({
         organizedBy: node,
+        nodeId: "s=" + nodeName + "|" + item.dn,
         browseName: item.dn,
         displayName: item.name + " (" + item.dn + ")"
       });
-        for (const property in item.props) {
-          if (item.props[property].op == 'calc' || item.props[property].op == 'par' || item.props[property].op == 'rw' || item.props[property].op == 'evnt') {
-            let dataType = {};
-            if (item.props[property].vtype == 'N') { dataType.s = 'Double'; dataType.obj = DataType.Double; }
-            if (item.props[property].vtype == 'S') { dataType.s = 'String'; dataType.obj = DataType.String; }
-            if (item.props[property].vtype == 'B') { dataType.s = 'Boolean'; dataType.obj = DataType.Boolean; }
-            namespace.addVariable({
-              componentOf: device,
-              browseName: property + " (" + item.dn + ")",
-              dataType: dataType.s,
-              description: item.props[property].name,
-              value: {
-                get: () => new Variant({ dataType: dataType.obj, value: dataType.s == 'Boolean' ? filter.devices[item._id + "." + property].value == 1 ? true : false : filter.devices[item._id + "." + property].value }),
-                set: (variant) => {
-                  if (variant.dataType == 1) {
-                    filter.devices[item._id + "." + property].value = variant.value == true ? 1 : 0;
-                  }
-                  if (variant.dataType == 11) {
-                    filter.devices[item._id + "." + property].value = parseFloat(variant.value);
-                  }
-                  if (variant.dataType == 12) {
-                    filter.devices[item._id + "." + property].value = String(variant.value);
-                  }
-                  plugin.send({ type: 'command', command: 'setval', did: item._id, prop: property, value: filter.devices[item._id + "." + property].value });
-                  return StatusCodes.Good;
+      for (const property in item.props) {
+        if (item.props[property].op == 'calc' || item.props[property].op == 'par' || item.props[property].op == 'rw' || item.props[property].op == 'evnt') {
+          let dataType = {};
+          if (item.props[property].vtype == 'N') { dataType.s = 'Double'; dataType.obj = DataType.Double; }
+          if (item.props[property].vtype == 'S') { dataType.s = 'String'; dataType.obj = DataType.String; }
+          if (item.props[property].vtype == 'B') { dataType.s = 'Boolean'; dataType.obj = DataType.Boolean; }
+          namespace.addVariable({
+            componentOf: device,
+            nodeId: "s=" + nodeName + "|" + item.dn + "." + property,
+            browseName: property + " (" + item.dn + ")",
+            dataType: dataType.s,
+            description: item.props[property].name,
+            value: {
+              get: () => new Variant({ dataType: dataType.obj, value: dataType.s == 'Boolean' ? filter.devices[item._id + "." + property].value == 1 ? true : false : filter.devices[item._id + "." + property].value }),
+              set: (variant) => {
+                if (variant.dataType == 1) {
+                  filter.devices[item._id + "." + property].value = variant.value == true ? 1 : 0;
                 }
+                if (variant.dataType == 11) {
+                  filter.devices[item._id + "." + property].value = parseFloat(variant.value);
+                }
+                if (variant.dataType == 12) {
+                  filter.devices[item._id + "." + property].value = String(variant.value);
+                }
+                plugin.send({ type: 'command', command: 'setval', did: item._id, prop: property, value: filter.devices[item._id + "." + property].value });
+                return StatusCodes.Good;
               }
-            });
-          }
-        }     
+            }
+          });
+        }
+        if (item.props[property].op == 'cmd') {
+          const method = namespace.addMethod(device, {
+            componentOf: device,
+            nodeId: "s=" + nodeName + "|" + item.dn + "." + property,
+            browseName: property + "()" + " (" + item.dn + ")",
+            description: item.props[property].name
+          });
+          method.bindMethod((callback) => {
+            plugin.send({ type: 'command', command: 'device', did: item._id, prop: property });
+            const callMethodResult = {
+              statusCode: StatusCodes.Good
+            };
+            callback(null, callMethodResult);
+          });
+        }
+      }
     })
   }
 
-  
+
   function subExtraChannels(filter) {
     plugin.onSub('devices', filter, data => {
       plugin.log("data" + util.inspect(data), 2);
       data.forEach(item => {
         if (filter.devices[item.did + '.' + item.prop] != undefined) {
           filter.devices[item.did + '.' + item.prop].value = item.value;
-        }      
-       });
+        }
+      });
     })
   }
 
   plugin.onChange('extra', async (recs) => {
     plugin.log('onChange addExtra ' + util.inspect(recs), 2);
-    /*extraChannels = await plugin.extra.get();
-    filter = filterExtraChannels(extraChannels)
-    subExtraChannels(filter);
-    addOpcDevices(filter.devicesarr, devicesNode);
-    addOpcDevices(filter.tagarr, tagNode);
-    addOpcDevices(filter.locationarr, locationNode);*/
+    let curfilter = {
+      did_prop: [],
+      devices: {},
+      locationobj: {},
+      tagobj: {},
+      deviceobj: {}
+    }
+    let curdevices = [];
+    for (let index = 0; index < recs.length; index++) {
+      if (recs[index].op == 'add') {
+        if (recs[index].filter == 'location') {
+          const location = recs[index].locationStr
+          const devices = await plugin.devices.get({ location: location});
+          const group = groupBy(devices, 'location');
+          curfilter.locationobj[location] = group;
+          curdevices.push(...devices);          
+          addOpcLocation(curfilter.locationobj);
+        }
+
+        curdevices.forEach(item => {
+          for (const property in item.props) {
+            curfilter.did_prop.push(item._id + "." + property);
+            curfilter.devices[item._id + "." + property] = item.props[property];
+          }
+        })
+        subExtraChannels(curfilter);
+        
+      }
+    }
+    //plugin.log('filter ' + util.inspect(filter.locationobj, null, 4));
+    //const addExtra = filterExtraChannels(recs);
+    /*for (const prop in filter.locationobj) { 
+      for (const prop1 in filter.locationobj[prop]) { 
+        plugin.log('NodeId ' + filter.locationobj[prop][prop1])
+      
+      }
+      
+    }*/
+    //namespace.deleteNode(devicesNode);
+
+
+    //filter = await filterExtraChannels(extraChannels);
+
+
+//    subExtraChannels(filter);
   });
 
   server.start(function () {
-    plugin.log("Server is now listening ... ");
-    plugin.log("port " + server.endpoints[0].port);
+    plugin.log("Server is now listening ... ", 1);
+    plugin.log("port " + server.endpoints[0].port, 1);
     const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
-    plugin.log(" the primary server endpoint url is " + endpointUrl);
+    plugin.log(" the primary server endpoint url is " + endpointUrl, 1);
   });
   // set the server to answer for modbus requests
 
@@ -121,32 +252,35 @@ module.exports = async function (plugin) {
     let res = {
       did_prop: [],
       devices: {},
-      devicesarr: [],
-      locationarr: [],
-      tagarr: []
+      devicesobj: {},
+      locationobj: {},
+      tagobj: {}
     };
-    
+
     const groupchannels = groupBy(channels, 'filter');
     for (const element in groupchannels) {
       let curdevices = [];
       if (element == 'device') {
         const devices = await plugin.devices.get({ did: groupchannels[element].didarr });
-        plugin.log("devices " + util.inspect(devices, null, 4));
-        res.devicesarr = devices;
+        res.devicesobj["Devices"] = devices;
         curdevices.push(...devices);
       }
       if (element == 'location') {
         for (let i = 0; i < groupchannels[element].ref.length; i++) {
-          const devices = await plugin.devices.get({ location: groupchannels[element].ref[i].locationStr });
-          res.locationarr.push(...devices);
+          const location = groupchannels[element].ref[i].locationStr
+          const devices = await plugin.devices.get({ location: location });
+          const group = groupBy(devices, 'location');
+          res.locationobj[location] = group;
           curdevices.push(...devices);
         }
       }
       if (element == 'tag') {
         for (let i = 0; i < groupchannels[element].ref.length; i++) {
-          const devices = await plugin.devices.get({ tag: groupchannels[element].ref[i].tagStr });
-          res.tagarr.push(...devices);
-          curdevices.push(...devices);          
+          const tag = groupchannels[element].ref[i].tagStr;
+          const devices = await plugin.devices.get({ tag: tag });
+          res.tagobj[tag] = [];
+          res.tagobj[tag].push(...devices);
+          curdevices.push(...devices);
         }
       }
       curdevices.forEach(item => {
@@ -156,6 +290,7 @@ module.exports = async function (plugin) {
         }
       })
     }
+    //plugin.log(" res " + util.inspect(res, null, 4));
     return res
   }
 
@@ -176,5 +311,22 @@ module.exports = async function (plugin) {
       return acc;
     }, {});
   }
+
+  function getDepth(loc) {
+    // '/place/dg003/dg010/'=> 2
+    const arr = loc.split('/');
+    return arr.length > 3 ? arr.length - 3 : 0;
+  }
+
+  function getParentLocation(loc) {
+    // '/place/dg003/dg010/'=> '/place/dg003/'
+    return (
+      loc
+        .split('/')
+        .slice(0, -2)
+        .join('/') + '/'
+    );
+  }
+
 
 };
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const util = require("util");
-
-//const plugin = require("ih-plugin-api")();
 const opcua_server = require("./app");
 
 (async () => {  
@@ -10,22 +8,22 @@ const opcua_server = require("./app");
     const opt = getOptFromArgs();
     const pluginapi = opt && opt.pluginapi ? opt.pluginapi : 'ih-plugin-api';
     plugin = require(pluginapi+'/index.js')();
-    plugin.log("OPCUA server plugin has started.");
+    plugin.log("OPCUA server plugin has started.", 1);
     plugin.params = await plugin.params.get();
     plugin.log('Received params '+ util.inspect(plugin.params));
      // Получить каналы для публикации
      plugin.extraChannels = await plugin.extra.get();
-     plugin.log('Received extra channels...');
+     plugin.log('Received extra channels...', 1);
     if (plugin.extraChannels.length > 0) {
-      plugin.log(`Received ${plugin.extraChannels.length} extra channels...`);
+      plugin.log(`Received ${plugin.extraChannels.length} extra channels...`, 1);
     } else {
-      plugin.log('Empty extra channels list!');
+      plugin.log('Empty extra channels list!', 1);
       process.exit(2);
     }
 
     await opcua_server(plugin);
   } catch (err) {
-    plugin.exit(8, `Error! Message: ${util.inspect(err)}`);
+    plugin.exit(8, `Error! Message: ${util.inspect(err)}`, 1);
   }
 })();
 
